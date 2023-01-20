7fe70dd8b4391c9b36c259839a70059526976074
Rita Chien <ritachien929@gmail.com>
2023-01-20 16:16:24 +0800

    feat: try utc time

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ dayjs.extend(timezone)
 
 const app = express()
 
-const now = dayjs()
-const time1 = dayjs().valueOf().tz('Asia/Taipei')
-const time2 = dayjs.tz(now.valueOf(), 'Asia/Taipei')
-const taipeiTime = dayjs().add(8, 'hours').format('YYYY-MM-DD HH:mm:ss')
-
+const now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+// const time1 = dayjs.valueOf().tz('Asia/Taipei')
+// const time2 = dayjs.tz(now.valueOf(), 'Asia/Taipei')
+const taipeiTime = dayjs.utc().add(8, 'hours').format('YYYY-MM-DD HH:mm:ss')
 app.get('/', (req, res) => {
   res.send({
     status: 'OK',
@@ -22,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 
-console.log(`${now}(now)\n${time1}(time1)\n${time2}(time2)\n${taipeiTime}(taipeiTime)`)
+console.log(`${now}(serverTime)\n${taipeiTime}(taipeiTime)`)
 
 app.listen(3000, () => {
   console.log('app is running on port 3000')
